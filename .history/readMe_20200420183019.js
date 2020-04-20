e3c2dffc6faf8d7d11da2741a0682f11da1909a7
Ky Kyler <ky.kyler1@gmail.com>
2020-04-20 19:02:49 -0400

    finished HW

---
+++ .history/readMe_20200420183019.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+// const generateRead = require("./utils/generateRead");
+ 
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?"
+  },
+  {
+    type: "input",
+    name: "URL to Project",
+    message: "the URL to your project?"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project's name?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test"
+  },
+  
+];
+  
+
+
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+   inquirer.prompt(questions).then((inquirerResponses) => {
+    writeToFile("README.md", generateRead({ ...inquirerResponses }));
+  })
+}
+
+init();
